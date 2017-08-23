1997dcde1cff6e610e0d062d2bb584bd6cfdd68f
OliveKong <2072446908@qq.com>
2017-08-23 14:27:49 +0800

    mobile scroll js

---
+++ js/rem.js
@@ -0,0 +1,15 @@
+(function(doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function() {
+            var clientWidth = docEl.clientWidth;
+            // add by myself
+            if (clientWidth > 640) clientWidth = 640;
+            // add by myself
+            if (!clientWidth) return;
+            docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
